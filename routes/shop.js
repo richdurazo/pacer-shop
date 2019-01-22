49e3c4f45e3eeb88f13a203300eaa680a44b1095
Richard Durazo <rcdurazo@gmail.com>
2019-01-21 18:29:04 -0800

    just messing around

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,18 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
   console.log(adminData.products)
   const products = adminData.products;
-  res.sendFile(path.join(rootDir,'views', 'shop.html'));
+  res.render('shop', { 
+    prods: products,
+    docTitle: 'My Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
