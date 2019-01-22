c9da8c1bad8fd8e7257898086cb31b6597ec40b9
Richard Durazo <rcdurazo@gmail.com>
2019-01-21 17:08:25 -0800

    just messing around

---
+++ first-app.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  console.log('time is done')
+}, 2000)
+
+console.log(d)
---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products)
+  const products = adminData.products;
+  res.sendFile(path.join(rootDir,'views', 'shop.html'));
+});
+
+module.exports = router;
